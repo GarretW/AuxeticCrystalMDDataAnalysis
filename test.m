2c895446cee40781a776a851806554d3828f7a93
Garret <gwieseh@gmail.com>
2018-10-15 15:33:45 -0700

    Add ordinal permutation to test space

--- test.m
+++ test.m
@@ -38,74 +38,83 @@ reg = regr(y,m,tau);        % Optimized regressor
 
 cpi = m*tau;                % Compression index
 
-%% Monty Carlo Method: Max Ent Critical Point Determination
+%% Ordinal permutation
+regi = zeros(size(reg));
 
-% nb = 4;                         % Number of bins
-% ncp = nb+1;                     % Number of critical points
-% 
-% conv = false;                   % Convergence flag
-% max_ent = 0;                    % Maximum entropy preallocation
-% max_cp = zeros(1,ncp);
-% 
-% while conv == false
-% 
-%         xc = sort(randi(yl,[ncp,1]));
-%         xc(1) = 1; xc(end) = yl;
-%         tmp_ent = cgment(y,reg,xc,cpi);        
-% 
-%         if tmp_ent > max_ent
-% 
-%             if abs(tmp_ent - max_ent) <= .01
-%                 conv = true;      
-%             end
-% 
-%             max_ent = tmp_ent;
-%             max_cp = xc;            
-%         end                    
-% end
+for i = cpi+1:yl
+    a = reg(:,i);
+    b = sort(a,'ascend');
+    
+    for j = 1:m
+        a(a==b(j)) = j;
+    end
+    regi(:,i) = a;
+end
+
+wrd = zeros(1,yl);
+
+for i = cpi+1:yl
+    tmp = 0;
+    for j = 1:m       
+        tmp = tmp + regi(j,i)*10^(m-j);
+    end
+    wrd(i) = tmp;
+end
+
+w = sort(wrd(:,cpi+1:end),'ascend');
+[w,~,ind] = unique(w);
+wct = accumarray(ind,1);
+
+wrd = [sort(w,'ascend');wct'];
+prb = (wct'/(yl-cpi));
+
+ent = 0;
+for i = 1:length(prb)
+    ent = ent - (prb(i)*log(prb(i)));
+end
 
 
 %%
-mci = 5;              % Monte Carlo iterations
-ncp = 3;                % Number of critical points
-% ent = zeros(1,mci);     % Entropy vector
-xc = zeros(ncp,mci);    % Critical point solution vector 
-
-max_ent = zeros(1,ncp);
-
-mcp = 150;
-cval = zeros(10,mcp);                                           % Target fit coefficients 
-
-for k = 1:10                                                    % Test multiplier    
-    for j = 3:mcp                                               % Crit point count
-        ent = zeros(1,mci);
-        
-        for i = 1:mci*k                                           % MC search
-            
-            tmp_xc = sort(randi(yl,[j,1]));                     % Generate j random crit points
-            tmp_xc(1) = 1; tmp_xc(end) = yl;
-            ent(i) = cgment(y,reg,tmp_xc,cpi);                  % Calculate entropy for solution and store
-            % xc(:,i) = tmp_xc;
-            
-        end
-        
-        max_ent(j) = max(ent);                                  % Store maximum entropy from best solution of j crit points
-        % a = xc(max(ent) == ent);                              % Store maximum entropy critical point index solutions
-        % max_xc(1:j,j) = a(1);
-        
-        clc;
-        fprintf('cpn: %0.f ',j);
-                
-    end
-    fprintf('\nDone: %0.f\n', k);
-    
-    for o = 6:mcp
-        tmp_c = coeffvalues(fit((3:o)',max_ent(3:o)','poly2'));
-        cval(k,o) = tmp_c(3);
-        
-    end
-    
-end
+% mci = 5;                                                        % Monte Carlo iterations
+% ncp = 3;                                                        % Number of critical points
+% % ent = zeros(1,mci);                                           % Entropy vector
+% xc = zeros(ncp,mci);                                            % Critical point solution vector 
+% 
+% max_ent = zeros(1,ncp);                                         % Maxima of MC entropy for given critical points
+% 
+% mcp = 150;
+% cval = zeros(10,mcp);                                           % Target fit coefficients 
+% 
+% for k = 1:10                                                    % Test multiplier    
+%     for j = 3:mcp                                               % Crit point count
+%         ent = zeros(1,mci);
+%         
+%         for i = 1:mci*k                                         % MC search
+%             
+%             tmp_xc = sort(randi(yl,[j,1]));                     % Generate j random crit points
+%             tmp_xc(1) = 1; tmp_xc(end) = yl;
+%             ent(i) = cgment(y,reg,tmp_xc,cpi);                  % Calculate entropy for solution and store
+%             % xc(:,i) = tmp_xc;
+%             
+%         end
+%         
+%         max_ent(j) = max(ent);                                  % Store maximum entropy from best solution of j crit points
+%         % a = xc(max(ent) == ent);                              % Store maximum entropy critical point index solutions
+%         % max_xc(1:j,j) = a(1);
+%         
+%         clc;
+%         fprintf('cpn: %0.f ',j);
+%                 
+%     end
+%     fprintf('\nDone: %0.f\n', k);
+%     
+%     for o = 6:mcp
+%         tmp_c = coeffvalues(fit((3:o)',max_ent(3:o)','poly2'));
+%         cval(k,o) = tmp_c(3);
+%         
+%     end
+%     
+% end
 
 
 %% Brute Force Method
@@ -119,31 +128,31 @@ end
 
 %% Plotting Worspace
 
-figure
-for i = 1:10 
-    hold on
-    subplot(2,1,1)
-    plot(cval(i,3:end))
-    
-    hold on
-    subplot(2,1,2)
-    plot(diff(cval(i,3:end)))
-    
-end
-hold on 
-subplot(2,1,2)
-plot(.01*ones(1,mcp-3),'-k','LineWidth',2);
-hold on
-subplot(2,1,1)
-xlabel('Included critical points')
-ylabel('C coeff')
-title('Maximum entropy fit analysis')
-
-hold on
-subplot(2,1,2)
-xlabel('Included critical points')
-ylabel('delta C coeff')
-title('Convergence')
+% figure
+% for i = 1:10 
+%     hold on
+%     subplot(2,1,1)
+%     plot(cval(i,3:end))
+%     
+%     hold on
+%     subplot(2,1,2)
+%     plot(diff(cval(i,3:end)))
+%      
+% end
+% hold on 
+% subplot(2,1,2)
+% %plot(.01*ones(1,mcp-3),'-k');
+% hold on
+% subplot(2,1,1)
+% xlabel('Included critical points')
+% ylabel('C coeff')
+% title('Maximum entropy fit analysis')
+% 
+% hold on
+% subplot(2,1,2)
+% xlabel('Included critical points')
+% ylabel('delta C coeff')
+% title('Convergence')
 
 %% Notes
 
